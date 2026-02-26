5944e4a7a8716359cc0eba902fe091aae869b274
njeruthuo <juliusn411@gmail.com>
2026-02-26 21:47:14 +0300

    feat: added regexp to help add security when requesting for files to prevent command injection

--- wiki.go
+++ wiki.go
@@ -1,17 +1,30 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
+	"regexp"
 	"strings"
 )
 
 type Page struct {
 	Title string
 	Body  []byte
+}
+
+var validPath = regexp.MustCompile("^/(edit|view|save)/([a-zA-Z0-9]+)$")
+
+func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
+	m := validPath.FindStringSubmatch(r.URL.Path)
+	if m == nil {
+		http.NotFound(w, r)
+		return "", errors.New("invalid page title")
+	}
+	return m[2], nil
 }
 
 var templates = template.Must(template.ParseFiles(
@@ -48,7 +61,10 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/view/"):]
+	title, err := getTitle(w, r)
+	if err != nil {
+		return
+	}
 	p, err := loadPage(title)
 
 	if err != nil {
@@ -60,7 +76,10 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/edit/"):]
+	title, err := getTitle(w, r)
+	if err != nil {
+		return
+	}
 
 	p, err := loadPage(title)
 
@@ -72,13 +91,16 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func saveHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/save/"):]
+	title, err := getTitle(w, r)
+	if err != nil {
+		return
+	}
 	body := r.FormValue("body")
 
 	// Here, we will need to check if the file already exist before creating a new one...
 
 	p := &Page{Title: title, Body: []byte(body)}
-	err := p.save()
+	err = p.save()
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
